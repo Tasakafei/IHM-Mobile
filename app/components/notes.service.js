29596abb5f7f16136e81c241ee808cd8dafe6d84
Alexandre CAZALA <alexandre.cazala@gmail.com>
2017-01-16 08:08:22 +0100

    scripts npm

--- app/components/notes.service.js
+++ app/components/notes.service.js
@@ -22,8 +22,9 @@ delete $http.defaults.headers.common['X-Requested-With'];
     	console.log(socket);
     	var i;
     	socket.emit("notes", notes, function () {
-    		console.log("pushed")
-    	})
+    		console.log("pushed");
+			callbackFunc();
+    	});
     	/*io.socket.post("/notes",notes, function(body,JWR){
 	        	      console.log('Sails responded with: ', body);
 	        	      notes = [];
@@ -37,7 +38,7 @@ delete $http.defaults.headers.common['X-Requested-With'];
 	        });
 	        notes = [];
     	}*/
-    };
+    }
 
 	return {
 		add: add,
